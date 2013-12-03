f93a05b9db6d39d850907cb2c9f4b72b5c263f1e
Marcel Vanderschaeghe <developer@developer.local>
2013-12-03 21:12:58 +0100

    zweiter upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html lang="de" xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
+	
+	<head>
+		<title>MOOC WebEng I: Aufgabe Kapitel 7 - Marcel Vanderschaeghe</title>
+		<link rel="icon" href="http://www.evs-eos.de/image/eos.ico" />
+		<link rel="stylesheet"  type="text/css" href="kap7.css" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	</head>
+	
+	<body>
+		<h1>Hausaufgabe Kapitel 7: Digitale Barrierfreiheit</h1>
+		<h2><a href="http://wave.webaim.org/report#/vanderschaeghem.cloudcontrolled.com" onclick="window.open(this.href);return false;"><img src="wcag2AAA.gif" alt="WAVE Tool for WAI-AAA WCAG 2.0 validation"/></a></h2>
+
+		<form action="http://de.wikipedia.org/wiki/Spezial:Suche">
+			<fieldset>
+				<legend>Suchen bei Wikipedia</legend>
+				<label for="search">Suchbegriff</label>
+				<input type="text" name="search" value="" id="search"/>
+				<input type="submit" name="go" value="Artikel" alt="Suche nur in Artikeln"/>
+				<input type="submit" name="fulltext" value="Volltext" alt="Volltextsuche" />
+			</fieldset>
+		</form>
+		
+		
+		
+		<h2>Marcel Vanderschaeghe, Dezember 2013</h2>
+		
+	</body>
+</html>
